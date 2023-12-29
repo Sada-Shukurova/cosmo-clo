d1994343caa3800ee53aa8d536c3ab464633ebc4
sada-shukurova <shukurova.sada@gmail.com>
2023-12-29 15:05:51 +0400

    product context

---
+++ src/contexts/ProductContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState, useEffect } from "react";
+// create context
+export const ProductContext = createContext();
+
+const ProductProvider = ({ children }) => {
+  // product state
+  const [products, setProducts] = useState([]);
+  // fetch products
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const resp = await fetch("https://fakestoreapi.com/products");
+      const data = await resp.json();
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductProvider;
