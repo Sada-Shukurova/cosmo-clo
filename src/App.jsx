52bcdf81d5a820f8d6946274b5bac965af2e24af
sada-shukurova <shukurova.sada@gmail.com>
2023-12-29 15:04:40 +0400

    app

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// import react-router
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import pages
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+// import components
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className="overflow-hidden">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="product/:id" element={<ProductDetails />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
