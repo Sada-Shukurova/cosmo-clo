16594ca1fbb27e0b85431a259a24afa170162460
sada-shukurova <shukurova.sada@gmail.com>
2023-12-29 15:05:01 +0400

    footer add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto">
+        <p className="text-center text-white">
+          Copyright &copy; Ecommerce Shop 2024. All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
