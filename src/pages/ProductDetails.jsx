35205312628eb996daa2ac56596fdd3e7d9b25e7
sada-shukurova <shukurova.sada@gmail.com>
2024-01-05 14:39:34 +0400

    add to cart

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -41,7 +41,21 @@ const ProductDetails = () => {
             />
           </div>
           {/* text */}
-          <div>text</div>
+          <div className="flex-1 text-center lg:text-left">
+            <h1 className="text-[26px] font-medium mb-2 max-w-[450px] mx-auto">
+              {title}
+            </h1>
+            <div className="text-xl text-red-500 font-medium mb-6">
+              $ {price}
+            </div>
+            <p className="mb-8">{description}</p>
+            <button
+              onClick={() => addToCart(product, product.id)}
+              className="bg-primary py-4 px-8 text-white"
+            >
+              Add to cart
+            </button>
+          </div>
         </div>
       </div>
     </section>
