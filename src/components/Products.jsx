e67c1cad4cd1941d84bc71f94ee4bc7269acd2d8
sada-shukurova <shukurova.sada@gmail.com>
2024-01-05 15:52:59 +0400

    products component added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -13,15 +13,17 @@ const Products = () => {
     );
   });
   return (
-    <section className="py-16">
-      <div className="container mx-auto px-10">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
-          {filteredProducts.map((product) => {
-            return <Product product={product} key={product.id} />;
-          })}
+    <div>
+      <section className="py-16">
+        <div className="container mx-auto px-10">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
+            {filteredProducts.map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
