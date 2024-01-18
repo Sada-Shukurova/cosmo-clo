f1415d0c741f1d51bddded2fa1e09d90681bec94
sada-shukurova <shukurova.sada@gmail.com>
2024-01-18 12:05:27 +0400

    add use icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import { SidebarContext } from "../contexts/SidebarContext";
 import { CartContext } from "../contexts/CartContext";
 // import icons
 import { BsBag } from "react-icons/bs";
+import { FiUser } from "react-icons/fi";
 // import Logo
 import Logo from "../img/logo.svg";
 
@@ -71,6 +72,11 @@ const Header = () => {
             </NavLink>
           );
         })}
+
+        {/* user */}
+        <Link to={"https://www.google.com"}>
+          <FiUser className="text-2xl" />
+        </Link>
         {/* cart */}
         <div
           onClick={() => setIsOpen(!isOpen)}
