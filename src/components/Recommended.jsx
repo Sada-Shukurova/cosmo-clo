2b2cb13a2c92d73ae783be8114fd8d7b0a83298c
sada-shukurova <shukurova.sada@gmail.com>
2024-01-18 11:13:36 +0400

    added recommented sections

---
+++ src/components/Recommended.jsx
@@ -0,0 +1,24 @@
+import Button from "./Button";
+
+const recommendedBtnClasses =
+  "bg-red-400 text-white font-semibold py-3 px-10 rounded-full";
+
+const Recommended = () => {
+  return (
+    <div>
+      <div className="container mx-auto px-10">
+        <h2 className="text-xl py-10">Recommended</h2>
+        {/* recommended-btns */}
+        <div className="flex justify-between items-center">
+          <Button text="All Products" classes={recommendedBtnClasses} />
+          <Button text="T-shirts" classes={recommendedBtnClasses} />
+          <Button text="Jewelery" classes={recommendedBtnClasses} />
+          <Button text="Bags" classes={recommendedBtnClasses} />
+          <Button text="Jeans" classes={recommendedBtnClasses} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Recommended;
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,13 @@
 import HeroSmall from "../components/HeroSmall";
 import Products from "../components/Products";
 import FilterSortSearch from "../components/FilterSortSearch";
+import Recommended from "../components/Recommended";
 
 const ProductsPage = () => {
   return (
     <div>
       <HeroSmall heroTitle={"Products"} />
+      <Recommended />
       <FilterSortSearch />
       <Products />
     </div>
