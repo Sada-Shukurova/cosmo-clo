a4c27db25dabd2bdbfbfae819d5b378d9e62bbf4
sada-shukurova <shukurova.sada@gmail.com>
2024-01-23 17:32:33 +0400

    add mui select element for sorting

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import React, { useState } from "react";
 
 const Sort = () => {
-  return <div>Sort</div>;
+  const [sortItems, setSortItems] = useState("none");
+
+  const handleChange = (event) => {
+    setSortItems(event.target.value);
+  };
+  return (
+    <div>
+      <FormControl
+        sx={{
+          minWidth: 150,
+          "& .MuiOutlinedInput-root": {
+            "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+              borderColor: "#E77975",
+            },
+            "&:hover .MuiOutlinedInput-notchedOutline": {
+              borderColor: "#E77975",
+            },
+          },
+          "& .MuiOutlinedInput-notchedOutline": {
+            border: "none",
+            boxShadow: "none",
+          },
+          "& .MuiSelect-icon": {
+            color: "white",
+          },
+        }}
+      >
+        <Select
+          labelId="sort-label"
+          id="simple-sort"
+          value={sortItems}
+          label="Sort"
+          onChange={handleChange}
+          sx={{
+            "& .MuiInputBase-input": {
+              backgroundColor: "#E77975",
+              color: "#fff ",
+              overflow: "visible",
+            },
+          }}
+        >
+          <MenuItem
+            sx={{ "&:hover": { backgroundColor: "#E77975" } }}
+            value={"none"}
+          >
+            Sort
+          </MenuItem>
+          <MenuItem
+            sx={{ "&:hover": { backgroundColor: "rgb(252 165 165)" } }}
+            value={"lowHigh"}
+          >
+            Price: Low-High
+          </MenuItem>
+          <MenuItem
+            sx={{ "&:hover": { backgroundColor: "rgb(252 165 165)" } }}
+            value={"highLow"}
+          >
+            Price: High-Low
+          </MenuItem>
+          <MenuItem
+            sx={{ "&:hover": { backgroundColor: "rgb(252 165 165)" } }}
+            value={"alphabet"}
+          >
+            Alphabetical
+          </MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 
 export default Sort;
