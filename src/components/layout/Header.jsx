7c746658158a9ab33426b9661939bf964db0ede8
Sada Shukurova <123108040+Sada-Shukurova@users.noreply.github.com>
2024-10-20 01:37:02 +0400

    adding mobile menu

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,32 +1,55 @@
+import { AnimatePresence, delay, motion } from "framer-motion";
 import { useContext, useEffect, useState } from "react";
-// sidebarcontext
 import { SidebarContext } from "../../contexts/SidebarContext";
-// card barcontext
 import { CartContext } from "../../contexts/CartContext";
-// import icons
 import { BsBag } from "react-icons/bs";
-import { FiUser } from "react-icons/fi";
-// import Logo
+// import { FiUser } from "react-icons/fi";
+import { HiMiniBars3, HiMiniBars3BottomRight } from "react-icons/hi2";
 import Logo from "/img/logo.svg";
-
-// import Link
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { myRoutes } from "../../Routes/MyRoutes";
 import { ProductContext } from "../../contexts/ProductContext";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
+const menuVariants = {
+  hidden: { opacity: 0, x: -300 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      staggerChildren: 0.5,
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+  exit: { opacity: 0, x: -300 },
+};
+const menuItemsVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      duration: 0.3,
+    },
+  },
+  exit: { opacity: 0, x: -100 },
+};
 const Header = () => {
-  // header state
   const [isActive, setIsActive] = useState(false);
   const { isOpen, setIsOpen } = useContext(SidebarContext);
   const { itemAmount } = useContext(CartContext);
-
-  // get products context
+  const [menuOpen, setMenuOpen] = useState(false);
   const { pageI, setPageI } = useContext(ProductContext);
 
-  // get location
   const location = useLocation();
 
-  // event listener
   useEffect(() => {
     window.addEventListener("scroll", () => {
       window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
@@ -35,7 +58,6 @@ const Header = () => {
 
   // location event listener
   useEffect(() => {
-    // Log a message when the path is "/"
     if (location.pathname === "/") {
       setPageI(false);
     } else if (location.pathname === "/products") {
@@ -75,14 +97,48 @@ const Header = () => {
             </NavLink>
           );
         })}
+        {/* burger */}
 
-        {/* user */}
-        {/* <Link to={"https://www.google.com"}>
-          <FiUser className="text-2xl" />
-        </Link> */}
+        <HiMiniBars3
+          onClick={() => setMenuOpen((prev) => !prev)}
+          className="block md:hidden text-2xl cursor-pointer"
+        />
+        {/* Mobile menu */}
+        <AnimatePresence>
+          {menuOpen && (
+            <motion.div
+              variants={menuVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              className="rounded-md sm:w-[50vw] w-full   p-5 pt-7 bg-white absolute top-20 flex flex-col gap-5 items-center sm:justify-evenly justify-start left-0  sm:h-[30vh] h-screen md:hidden"
+            >
+              {myRoutes.map(({ path, text, id }) => {
+                return (
+                  <motion.div variants={menuItemsVariants}>
+                    <NavLink
+                      onClick={() => setMenuOpen(false)}
+                      key={id}
+                      to={path}
+                      className={({ isActive }) =>
+                        `${
+                          isActive
+                            ? "text-red-700 border-b-2 border-b-red-700  font-semibold"
+                            : ""
+                        }  text-xl hover:scale-105 duration-500 transition-all`
+                      }
+                    >
+                      {text}
+                    </NavLink>
+                  </motion.div>
+                );
+              })}
+            </motion.div>
+          )}
+        </AnimatePresence>
         {/* cart */}
         <div
-          onClick={() => setIsOpen(!isOpen)}
+          onClick={() => setIsOpen((prev) => !prev)}
           className="cursor-pointer flex relative "
         >
           <BsBag className="text-2xl" />
@@ -90,6 +146,24 @@ const Header = () => {
             {itemAmount}
           </div>
         </div>
+        {/* clerk auth */}
+
+        <SignedOut>
+          <div className="bg-red-700 duration-500 transition-all hover:bg-red-900 text-white font-bold py-2 px-4 rounded">
+            <SignInButton />
+          </div>
+        </SignedOut>
+        <SignedIn>
+          <UserButton
+            appearance={{
+              elements: {
+                userButtonAvatarBox: "w-10 h-10",
+                userButtonTrigger:
+                  "flex items-center border border-gray-300 rounded-full p-1 hover:bg-gray-100",
+              },
+            }}
+          />
+        </SignedIn>
       </div>
     </header>
   );
